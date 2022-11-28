723a1a5be54840255f7f38441106922090cbf2e6
cadu <cadu@2km.com.br>
2022-11-28 01:54:57 -0300

    connect ballot

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,22 +2,24 @@ import { HttpException } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ethers, Wallet } from 'ethers';
-import { AlchemyProvider, InfuraProvider } from "@ethersproject/providers";
+import { AlchemyProvider, InfuraProvider } from '@ethersproject/providers';
 //import tokenJson from './assets/MyToken.json';
 import * as tokenJson from './assets/MyToken.json';
 
 @Injectable()
 export class AppService {
-
   provider: ethers.providers.Provider;
-  tokenVoteContractAddr : string;
+  tokenVoteContractAddr: string;
+  ballotConnected: boolean;
   signerWallet: Wallet;
 
-  constructor(private configService : ConfigService) {
-    const alchemyApiKey = this.configService.get<string>("ALCHEMY_API_KEY");
-    const walletPrivateKey = this.configService.get<string>("PRIVATE_KEY");
-    this.tokenVoteContractAddr = this.configService.get<string>("TOKENISED_VOTE_CONTRACT_ADDR");
-    this.provider = new AlchemyProvider("goerli", alchemyApiKey);
+  constructor(private configService: ConfigService) {
+    const alchemyApiKey = this.configService.get<string>('ALCHEMY_API_KEY');
+    const walletPrivateKey = this.configService.get<string>('PRIVATE_KEY');
+    this.tokenVoteContractAddr = this.configService.get<string>(
+      'TOKENISED_VOTE_CONTRACT_ADDR',
+    );
+    this.provider = new AlchemyProvider('goerli', alchemyApiKey);
     this.signerWallet = new Wallet(walletPrivateKey);
     //this.provider = ethers.getDefaultProvider("goerli");
   }
@@ -31,20 +33,42 @@ export class AppService {
     const walletAddress = this.signerWallet.address;
     const signer = this.signerWallet.connect(this.provider);
     const voteTokenContract = new ethers.Contract(
-      this.tokenVoteContractAddr, tokenJson.abi, signer);
+      this.tokenVoteContractAddr,
+      tokenJson.abi,
+      signer,
+    );
     const signedVoteTokenContract = voteTokenContract.connect(signer);
-    console.log(`minting using contract address ${signedVoteTokenContract.address}.`);
-    const mintTx = await signedVoteTokenContract.mint(mintToWalletAddress,
-      ethers.utils.parseEther("10") //TODO: replace this with a correct value
+    console.log(
+      `minting using contract address ${signedVoteTokenContract.address}.`,
+    );
+    const mintTx = await signedVoteTokenContract.mint(
+      mintToWalletAddress,
+      ethers.utils.parseEther('10'), //TODO: replace this with a correct value
     );
     console.log(`mint Tx .hash is ${mintTx.hash}`);
     const mintTxReceipt = await mintTx.wait();
-    console.log(`mintTxReceipt is ${mintTxReceipt.transactionHash} after awaiting mint Tx ${mintTx.hash}`);
-    console.log(`${mintTxReceipt.transactionHash} is tx hash for tokens minted for ${mintToWalletAddress}`);
-    return {result : mintTxReceipt.transactionHash};
+    console.log(
+      `mintTxReceipt is ${mintTxReceipt.transactionHash} after awaiting mint Tx ${mintTx.hash}`,
+    );
+    console.log(
+      `${mintTxReceipt.transactionHash} is tx hash for tokens minted for ${mintToWalletAddress}`,
+    );
+    return { result: mintTxReceipt.transactionHash };
   }
 
   getTokenAddress() {
-    return {result: this.tokenVoteContractAddr};
-  } 
+    return { result: this.tokenVoteContractAddr };
+  }
+
+  connectBallot(ballotContractAddress: string) {
+    const signer = this.signerWallet.connect(this.provider);
+
+    // TODO: Attach to the contract address
+    return {
+      result: [
+        { name: 'Proposal 1', id: '1' },
+        { name: 'Proposal 2', id: '2' },
+      ],
+    };
+  }
 }
